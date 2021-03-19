fe62723334386b94924ad97510890f2adf437447
guzin8 <kanburogluguzin@gmail.com>
2021-03-18 18:19:54 -0700

    dd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reCapProject';
+  title:string = 'reCapProject';
+  user:string = 'Güzin Kanburoğlu';
 }
---
+++ src/app/models/car.ts
@@ -0,0 +1,9 @@
+import { DecimalPipe } from "@angular/common";
+
+export interface Car{
+    carId:number;
+    brandName:string;
+    colorName:number;
+    modelYear:number;
+    dailyPrice:number;   
+}
